ba62595e57230de8661793a4a62c062638fbf192
richard <richard.millett+github@googlemail.com>
2021-04-14 18:10:33 +0100

    Added test for pet not found

---
+++ src/test/java/DataObjects/PetData.java
@@ -0,0 +1,14 @@
+package DataObjects;
+
+public class PetData {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- src/test/java/stepDefinitions/Pet.java
+++ src/test/java/stepDefinitions/Pet.java
@@ -1,24 +1,34 @@
 package stepDefinitions;
 
+import DataObjects.PetData;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
 
 public class Pet {
 
+    private final PetData petData;
+    private Response response;
+
+    public Pet(PetData petData) {
+        this.petData = petData;
+    }
+
     @Given("user has an invalid pet id")
     public void user_has_an_invalid_pet_id() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+        petData.setId(-1);
     }
+
     @When("user makes a request to GET by id")
     public void user_makes_a_request_to_get_by_id() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+        response = given().port(80).when().get("localhost/api/v3/pet/" + petData.getId());
     }
+
     @Then("response is not found")
     public void response_is() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+        response.then().statusCode(404);
     }
 }
